814712fc85073b715a9a790d6021af1c8a2b4745
Dexaran <dexaran820@gmail.com>
2025-04-16 09:54:36 +0100

    Update SimplifiedMultisig.sol

--- SimplifiedMultisig.sol
+++ SimplifiedMultisig.sol
@@ -1,1 +1,121 @@
-init
+// SPDX-License-Identifier: No License (None)
+pragma solidity ^0.8.0;
+
+contract CallistoMultisig {
+    
+    struct Tx
+    {
+        address to;
+        uint256 value;
+        bytes   data;
+        
+        uint256 proposed_timestamp;
+        bool    executed;
+        mapping (address => bool) signed_by;
+        
+        uint256 num_approvals;
+    }
+    
+    mapping (uint256 => Tx)   public txs;
+    mapping (address => bool) public owner;
+    uint256 public num_owners;
+    uint256 public vote_pass_threshold;
+    uint256 public num_TXs         = 0;
+    uint256 public execution_delay = 40 days;
+    
+    modifier onlyOwner
+    {
+        require(owner[msg.sender], "Only owner is allowed to do this");
+        _;
+    }
+    
+    constructor (address _owner1, address _owner2, address _owner3, address _owner4) {
+        owner[_owner1] = true;
+        owner[_owner2] = true;
+        owner[_owner3] = true;
+        owner[_owner4] = true;
+        num_owners     = 4;
+    }
+    
+    // Allow it to receive ERC223 tokens and Funds transfers.
+    receive() external payable { }
+    fallback() external payable { }
+    
+    function executeTx(uint256 _txID) public onlyOwner
+    {
+        require(txAllowed(_txID), "Tx is not allowed");
+        
+        address _destination = txs[_txID].to;
+        _destination.call{value:txs[_txID].value}(txs[_txID].data);
+        
+        txs[_txID].executed = true;
+    }
+    
+    function proposeTx(address _to, uint256 _valueInWEI, bytes calldata _data) public onlyOwner
+    {
+        num_TXs++;
+        // Setup Tx values.
+        txs[num_TXs].to    = _to;
+        txs[num_TXs].value = _valueInWEI;
+        txs[num_TXs].data  = _data;
+        
+        // Setup system values to keep track on Tx validity and voting.
+        txs[num_TXs].proposed_timestamp    = block.timestamp;
+        txs[num_TXs].signed_by[msg.sender] = true;
+        txs[num_TXs].num_approvals         = 1; // Well, the one who proposes it approves it obviously.
+    }
+    
+    function rejectTx(uint256 _txID) public onlyOwner
+    {
+        txs[_txID].signed_by[msg.sender] = true;
+        txs[_txID].executed              = true; // Mark Tx as already-executed
+    }
+    
+    function approveTx(uint256 _txID) public onlyOwner
+    {
+        require(!txs[_txID].signed_by[msg.sender], "This Tx is already signed by this owner");
+        txs[_txID].signed_by[msg.sender] = true;
+        txs[_txID].num_approvals++;
+        if(txs[_txID].num_approvals >= vote_pass_threshold)
+        {
+            executeTx(_txID);
+        }
+    }
+    
+    function txAllowed(uint256 _txID) public view returns (bool)
+    {
+        /**
+         * Allows Tx to be executed if ALL owners signed it
+         * or it was proposed 40 days ago and no one voted to reject it.
+         */
+        require(!txs[_txID].executed, "Tx already executed or rejected");
+        require(txs[_txID].num_approvals >= vote_pass_threshold || block.timestamp >= txs[_txID].proposed_timestamp + execution_delay, "Tx is not approved by at least one of owners");
+        return true;
+    }
+    
+    function addOwner(address _owner) public
+    {
+        require(msg.sender == address(this), "Only internal voting can introduce new owners");
+        owner[_owner] = true;
+        num_owners++;
+    }
+    
+    function removeOwner(address _owner) public
+    {
+        require(msg.sender == address(this), "Only internal voting can remove existing owners");
+        owner[_owner] = false;
+        num_owners--;
+    }
+    
+    function setupDelay(uint256 _newDelayInSeconds) public
+    {
+        require(msg.sender == address(this), "Only internal voting can adjust the delay");
+        execution_delay = _newDelayInSeconds;
+    }
+    
+    function setupThreshold(uint256 _newThreshold) public
+    {
+        require(msg.sender == address(this), "Only internal voting can adjust the delay");
+        vote_pass_threshold = _newThreshold;
+    }
+}
